c38fc5409cb7cdbdab5d79c99dbbf511705d2dde
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-11-02 13:43:29 -0700

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +18,28 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # DELETE /PLANTS
+  def destroy 
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end 
+
+  #patch /plants
+  def update 
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end 
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
+
 end
